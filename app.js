5a72bc82e67a5b3f18932a045751795251ab73db
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-11-19 14:03:43 -0800

    aded some styles and icons

---
+++ app.js
@@ -0,0 +1,2 @@
+//API-KEY : https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&cnt=3&APPID=eccf41dbcc7bcbfa72b4d979e3f4ebb4
+//Weather ICON : http://openweathermap.org/img/wn/${iconCode}@4x.png
